3290bd37707b65aff93286adb464399e7cc89fe1
takudzwa17 <takuemujo@gmail.com>
2021-09-17 17:29:58 +0200

    addition of the logging in option and giving a rating

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import UserProvider from './context';
+import Login from './components/Login';
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
 const App = () => (
   <Router>
+    <UserProvider>
     <Header />
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path='/:movieId' element={<Movie />} />
       <Route path='/*' element={<NotFound />} />
     </Routes>
     <GlobalStyle />
+    </UserProvider>
   </Router>
 );
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+import { Context } from '../../context';
 
-const Header = () => (
-  <Wrapper>
+const Header = () => {
+  const [user] = useContext(Context)
+  console.log(user);
+
+  return (
+    <Wrapper>
     <Content>
       <Link to='/'>
         <LogoImg src={RMDBLogo} alt='rmdb-logo' />
       </Link>
+      {user ? (
+        <span className="loggedIn">Logged in as: {user.username}</span>
+      ) : (
+        <Link to="/login">
+          <span className="login">Log in</span>
+        </Link>
+      )
+      }
       <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
     </Content>
   </Wrapper>
-);
+  )
+}
 
 export default Header;
